65714dd97966d71402b42a87e6bac4ea96107a94
vutomi99 <vutomi99.ngobeni@gmail.com>
2023-12-18 11:31:46 +0200

    local serve

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const express = require('express')
-const app = express()
-const dotenv = require ("dotenv");
+// const express = require('express');
+// const app = express();
+// const dotenv = require("dotenv");
+// const mongoose = require('mongoose');
+// const cors = require('cors'); // Add this line
+// const productRouter = require('./routes/products');
+
+// dotenv.config();
+// mongoose.connect(process.env.MONGO_URL).then(() => console.log("db Connected")).catch((err) => console.log(err));
+
+// app.use(cors()); // Add this line to enable CORS for all routes
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// // create endpoint
+// app.use('/api/products', productRouter);
+
+// app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
+
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
 const mongoose = require('mongoose');
-const productRouter = require('./routes/products')
-const port = 3000
+const cors = require('cors');
+const productRouter = require('./routes/products');
 
-dotenv.config()
-mongoose.connect(process.env.MONGO_URL).then(()=>console.log("db Connected")).catch((err)=>console.log(err))
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({limit: '10mb',extended: true}))
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("db Connected")).catch((err) => console.log(err));
 
-//create endpoint
-app.use('/api/products', productRouter)
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))   
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+// Replace 'localhost' with your local IP address
+const localIpAddress = '192.168.8.100'; 
+// create endpoint
+app.use(`/api/products`, productRouter);
+
+app.listen(process.env.PORT, localIpAddress, () => {
+  console.log(`Example app listening on http://${localIpAddress}:${process.env.PORT}!`);
+});
