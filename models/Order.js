b1ad8b0d86a64cb6a253eff12e9549ddb90a091a
vutomi99 <vutomi99.ngobeni@gmail.com>
2023-12-18 15:14:57 +0200

    Order Model

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 
 
 const  OrderSchema = new mongoose.Schema({
+   userId: {type: String, required:true},
+   customerId: {type: String, required:true},
+   productId: {
+    type: mongoose.Schema.ObjectId,
+    ref:"Product"
+   },
+   quantity: {type: Number, required:true},
+   subtotal: {type: Number, required:true},
+   total: {type: Number, required:true},
+   delivery_status: {type: String,default:"pending"},
+   payment_status: {type: Number, required:true},
+   
   
 },{timestamps:true});
 
