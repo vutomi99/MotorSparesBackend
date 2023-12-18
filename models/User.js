f5c7e95d66866b45401271e25fb495c34a18cf59
vutomi99 <vutomi99.ngobeni@gmail.com>
2023-12-18 14:47:00 +0200

    User Model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 
 const  UserSchema = new mongoose.Schema({
-  username: {type: String}
+    username: {type: String, required:true},
+    email: {type: String, required:true, unique:true},
+    password: {type: String, required:true},
+    location: {type: String,default: "pretoria"}
 },{timestamps:true});
 
 
